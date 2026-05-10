15e1df049889a80917bb0c9c2f717e2079d5cf66
Amal Yakubov <129682707+amalyakubov@users.noreply.github.com>
2026-05-10 14:47:29 +0200

    feat: implement auth -> refresh token + access token exchange

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,15 @@ use axum::response::{IntoResponse, Redirect};
 use axum::routing::get;
 use serde::Deserialize;
 use std::sync::Arc;
+use std::sync::Mutex;
 
+#[derive(Clone)]
 struct AppState {
-    access_token: Option<String>,
+    refresh_token: Arc<Mutex<Option<String>>>,
+    access_token: Arc<Mutex<Option<String>>>,
+    client_id: String,
+    redirect_uri: String,
+    client_secret: String,
 }
 
 #[derive(Debug, Deserialize)]
@@ -16,8 +22,20 @@ struct Params {
 
 #[tokio::main]
 async fn main() {
-    let shared_state = Arc::new(AppState { access_token: None });
     dotenv::dotenv().ok();
+    let client_id = dotenv::var("CLIENT_ID").expect("Unable to get client id env variable");
+    let redirect_uri =
+        dotenv::var("REDIRECT_URI").expect("Unable to get redirect uri env variable");
+    let client_secret =
+        dotenv::var("CLIENT_SECRET").expect("Unable to get client secret env variable");
+
+    let shared_state = AppState {
+        client_id: client_id,
+        redirect_uri: redirect_uri,
+        refresh_token: Arc::new(Mutex::new(None)),
+        access_token: Arc::new(Mutex::new(None)),
+        client_secret: client_secret,
+    };
     let app = Router::new()
         .route("/", get(index_handler))
         .route("/auth", get(auth_handler))
@@ -32,26 +50,37 @@ async fn main() {
 }
 
 // Request token from Google
-async fn index_handler(State(state): State<Arc<AppState>>) -> impl IntoResponse {
-    if state.access_token.is_none() {
+async fn index_handler(State(state): State<AppState>) -> impl IntoResponse {
+    if state.access_token.lock().unwrap().is_none() {
         return Redirect::to("/auth").into_response();
     }
 
     return "HEY".to_string().into_response();
 }
 
-async fn auth_handler() -> impl IntoResponse {
-    let client_id = dotenv::var("CLIENT_ID").unwrap();
-    let redirect_uri = dotenv::var("REDIRECT_URI").unwrap();
+async fn auth_handler(State(state): State<AppState>) -> impl IntoResponse {
+    let (client_id, redirect_uri) = (state.client_id, state.redirect_uri);
     let uri = format!(
         "https://accounts.google.com/o/oauth2/v2/auth?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code&scope=https://www.googleapis.com/auth/gmail.addons.current.message.readonly&access_type=offline"
     );
     return Redirect::to(&uri).into_response();
 }
 
-async fn auth_callback_handler(params: Query<Params>) -> impl IntoResponse {
-    let params: Params = params.0;
-    return params.code.into_response();
+async fn auth_callback_handler(params: Query<Params>, state: State<AppState>) -> impl IntoResponse {
+    let (client_id, redirect_uri, client_secret) =
+        (&state.client_id, &state.redirect_uri, &state.client_secret);
+    let access_token = params.0.code;
+
+    let client = reqwest::Client::new();
+
+    let uri = format!(
+        "https://oauth2.googleapis.com/token?client_id={client_id}&code={access_token}&grant_type=authorization_code&redirect_uri={redirect_uri}&client_secret={client_secret}"
+    );
+
+    match client.post(uri).send().await {
+        Ok(response) => response.text().await.unwrap().into_response(),
+        Err(e) => e.to_string().into_response(),
+    }
 }
 
 async fn get_list_of_emails(user_id: String) {}
