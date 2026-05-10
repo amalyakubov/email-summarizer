e9265a2f3b1a821e7aed9692af6ca614046f8c31
Amal Yakubov <129682707+amalyakubov@users.noreply.github.com>
2026-05-10 04:51:42 +0200

    fix: oauth flow not redirecting to consent screen correctly

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
 use axum::Router;
+use axum::extract::State;
+use axum::response::{Html, IntoResponse, Redirect, Result};
 use axum::routing::get;
+use std::sync::Arc;
 
-use axum::response::Html;
+struct AppState {
+    access_token: Option<String>,
+}
 
 #[tokio::main]
 async fn main() {
+    let shared_state = Arc::new(AppState { access_token: None });
     dotenv::dotenv().ok();
-    let app = Router::new().route(
-        "/",
-        get(|| async {
-            match get_token().await {
-                Ok(response) => Html(response),
-                Err(e) => Html(e.to_string()),
-            }
-        }),
-    );
+    let app = Router::new()
+        .route("/", get(index_handler))
+        .route("/auth/callback", get(auth_handler))
+        .with_state(shared_state);
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
         .expect("Failed to create a TCP listener");
@@ -23,15 +24,22 @@ async fn main() {
         .expect("Failed to serve the app");
 }
 
-// TODO: Finish implementing
-pub async fn get_token() -> reqwest::Result<String> {
+// Request token from Google
+async fn index_handler(State(state): State<Arc<AppState>>) -> impl IntoResponse {
+    if state.access_token.is_none() {
+        return Redirect::to("/auth/callback").into_response();
+    }
+
+    return "HEY".to_string().into_response();
+}
+
+async fn auth_handler() -> impl IntoResponse {
     let client_id = dotenv::var("CLIENT_ID").unwrap();
     let redirect_uri = dotenv::var("REDIRECT_URI").unwrap();
     let uri = format!(
-        "https://accounts.google.com/o/oauth2/v2/auth?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code&scope=https://www.googleapis.com/auth/gmail.addons.current.message.readonly"
+        "https://accounts.google.com/o/oauth2/v2/auth?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code&scope=https://www.googleapis.com/auth/gmail.addons.current.message.readonly&access_type=offline"
     );
-    let request = reqwest::get(uri).await?.text().await?;
-    return Ok(request);
+    return Redirect::to(&uri).into_response();
 }
 
 async fn get_list_of_emails(user_id: String) {}
