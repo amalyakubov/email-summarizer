48569256f2694ef875d8ab080b8e0fa00c0ec4c3
Amal Yakubov <129682707+amalyakubov@users.noreply.github.com>
2026-05-10 05:42:09 +0200

    feat: implement basic auth_callback

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use axum::Router;
-use axum::extract::State;
-use axum::response::{Html, IntoResponse, Redirect, Result};
+use axum::extract::{Query, State};
+use axum::response::{IntoResponse, Redirect};
 use axum::routing::get;
+use serde::Deserialize;
 use std::sync::Arc;
 
 struct AppState {
     access_token: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Params {
+    code: String,
 }
 
 #[tokio::main]
@@ -14,7 +20,8 @@ async fn main() {
     dotenv::dotenv().ok();
     let app = Router::new()
         .route("/", get(index_handler))
-        .route("/auth/callback", get(auth_handler))
+        .route("/auth", get(auth_handler))
+        .route("/auth/callback", get(auth_callback_handler))
         .with_state(shared_state);
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
@@ -27,7 +34,7 @@ async fn main() {
 // Request token from Google
 async fn index_handler(State(state): State<Arc<AppState>>) -> impl IntoResponse {
     if state.access_token.is_none() {
-        return Redirect::to("/auth/callback").into_response();
+        return Redirect::to("/auth").into_response();
     }
 
     return "HEY".to_string().into_response();
@@ -40,6 +47,11 @@ async fn auth_handler() -> impl IntoResponse {
         "https://accounts.google.com/o/oauth2/v2/auth?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code&scope=https://www.googleapis.com/auth/gmail.addons.current.message.readonly&access_type=offline"
     );
     return Redirect::to(&uri).into_response();
+}
+
+async fn auth_callback_handler(params: Query<Params>) -> impl IntoResponse {
+    let params: Params = params.0;
+    return params.code.into_response();
 }
 
 async fn get_list_of_emails(user_id: String) {}
